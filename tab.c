66b7bd036dba66c3aa33c36ae88fd73d0fe24458
Matmart12 <131346295+Matmart12@users.noreply.github.com>
2023-05-26 10:41:29 +0200

    Update tab.c

--- tab.c
+++ tab.c
@@ -869,7 +869,7 @@ char **Placeswitch(char **tab, int length, int width)
 
 
 
-int savefile(char** tab, int score, sign, int width, int length){
+ int savefile(int score){
    // Ouvrir le fichier highscore en mode lecture
     FILE *file = fopen("highscore.txt", "r");
     if (file == NULL) {
@@ -885,7 +885,7 @@ int savefile(char** tab, int score, sign, int width, int length){
         return 1;
     }
     char newName[256];//pour avoir le nom de la personne
-    printf("what's your name/surname?"\n);
+    printf("what's your name/surname?\n");
     scanf("%s", newName);
     // Lire le contenu du fichier highscore ligne par ligne
     char buffer[256];
@@ -894,16 +894,16 @@ int savefile(char** tab, int score, sign, int width, int length){
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
         char *name = strtok(buffer, ",");
         char *scoreStr = strtok(NULL, ",");
-        int score = atoi(scoreStr);
-        if (score < newScore && !scoreUpdated) {// Vérifier si le score actuel est inférieur au nouveau score
-            core = newScore;// Modifier le score
+        int scoreecr = atoi(scoreStr);
+        if (scoreecr < score && !scoreUpdated) {// Vérifier si le score actuel est inférieur au nouveau score
+            scoreecr = score;// Modifier le score
             strcpy(name, newName); // Mettre à jour le nom si nécessaire
             scoreUpdated = 1; // Indiquer que le score a été mis à jour
         }
         fprintf(tempFile, "%s,%d\n", name, score); // Écrire les données modifiées dans le fichier temporaire   
     }
     if (!scoreUpdated) {
-        fprintf(tempFile, "%s,%d\n", newName, newScore);// Si le score n'a pas encore été mis à jour et qu'il n'y a plus de scores à lire
+        fprintf(tempFile, "%s,%d\n", newName, score);// Si le score n'a pas encore été mis à jour et qu'il n'y a plus de scores à lire
     // Ajouter le nouveau score à la fin de la liste
     }
     fclose(file);
