272c700662304b00437906f365db32aaab6baca3
Matmart12 <131346295+Matmart12@users.noreply.github.com>
2023-05-27 15:45:19 +0200

    Update envoie.c

--- envoie.c
+++ envoie.c
@@ -1,1 +1,6 @@
+if((x!=x2+1 && x!=x2-1 && x!= x2) || (y!=y2+1 && y!=y2-1 && y!= y2)||(x==x2+1 && y==y2+1)||(x==x2-1 && y==y2+1)||(x==x2+1 && y==y2-1)||(x==x2-1 && y==y2-1)){
+            printf("Try again to switch caracter's places in 3 secondes.\n");
+        sleep(3);
+        }
+        } while ((x!=x2+1 && x!=x2-1 && x!= x2) || (y!=y2+1 && y!=y2-1 && y!= y2)||(x==x2+1 && y==y2+1)||(x==x2-1 && y==y2+1)||(x==x2+1 && y==y2-1)||(x==x2-1 && y==y2-1));
 
