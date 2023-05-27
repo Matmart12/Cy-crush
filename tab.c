f159d0723124a1169a01be9c629420ffb2350e81
Matmart12 <131346295+Matmart12@users.noreply.github.com>
2023-05-27 14:55:17 +0200

    Update tab.c

--- tab.c
+++ tab.c
@@ -4,7 +4,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <unistd.h>
-
+#include "main.h"
 #define CB_RED "\033[91m"
 #define CB_GREEN "\033[92m"
 #define CB_YELLOW "\033[93m"
@@ -1292,6 +1292,8 @@ int savehighscore(int score){
 
 
 void loadhighscore(){
+    int linemax=10;
+    int linepr=0;
      FILE *file = fopen("highscore.txt", "r"); //Ouvrir le fichier highscore en mode lecture
     if (file == NULL) {
         printf("Impossible to read the file.\n");
@@ -1299,8 +1301,9 @@ void loadhighscore(){
         return 1;
     }
     char line[256];
-    while(fgets(line,sizeof(line),file)!=NULL){
+    while(fgets(line,sizeof(line),file)!=NULL &&linepr<linemax){
         printf("%s", line);
+        linepr++;
     }
     fclose(file);
 }
@@ -1327,77 +1330,4 @@ void changeHour(int totalTime)
         seconds = (totalTime % 3600) % 60;
         printf("you played %d hour(s), %d minute(s) et %d second(s).\n\n",hours,minutes,seconds);
     }
-}
-
-
-
-void main()
-{
-    srand(time(NULL));
-    time_t begin = time(NULL);
-    int tab2[DEUX], length, width, sign, totalTime, roundNmb, score = 0, loadScore; // longueur, largeur, nombre de symboles
-    char** tab;
-    if(checkGame())
-    {
-        if(wantLoadGame())
-        {
-            length = loadLength();
-            width = loadWidth(); 
-            tab = malloc(width * sizeof(char*));
-            for (int i = 0; i < width; i++)
-            {
-                tab[i] = malloc(length * sizeof(char));
-            }
-            tab[width] = malloc(length * sizeof(int));
-            loadGame(tab,length,width);
-            roundNmb = roundNumber();
-        }
-        else
-        {
-            system("clear");
-            createLength_and_Width(tab2);
-            length = tab2[0];
-            width = tab2[1];
-            sign = createSign(tab2);
-            roundNmb = roundNumber();
-            tab = malloc(width * sizeof(char*));
-            for (int i = 0; i < width; i++)
-            {
-                tab[i] = malloc(length * sizeof(char));
-            }
-            tab[width] = malloc(length * sizeof(int));
-            buildTab(tab, length, width, sign);
-            destroyTabforStart(tab, length, width, sign);
-        }
-    }
-    else
-    {
-        system("clear");
-        createLength_and_Width(tab2);
-        length = tab2[0];
-        width = tab2[1];
-        sign = createSign(tab2);
-        roundNmb = roundNumber();
-        tab = malloc(width * sizeof(char*));
-        for (int i = 0; i < width; i++)
-        {
-            tab[i] = malloc(length * sizeof(char));
-        }
-        tab[width] = malloc(length * sizeof(int));
-        buildTab(tab, length, width, sign);
-        destroyTabforStart(tab, length, width, sign);
-    }
-    score = tab[width][0];
-    showTab(tab, length, width, score);
-    
-    if(length<=32 && width<=99)
-    {   
-        game(tab,length,width,sign,score,roundNmb);
-    }
-    savehighscore(score);
-    loadhighscore();
-    time_t end = time(NULL);
-    totalTime = end - begin;
-    changeHour(totalTime);
-    free(tab);
 }
